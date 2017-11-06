9fc72740af466902d60c12071863d563c2b93c1c
leksyib <leksyib14@gmail.com>
2017-11-06 15:39:57 +0100

    fixed router problems

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const controller1 = ('../controllers/controller1.js')
+const controller1 = require('../controllers/controller1.js')
 
 router.get('/', controller1.home);
 router.get('/signUp', controller1.signup);
