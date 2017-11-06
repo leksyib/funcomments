edd7d9f62da2da7f5f695c469dc17d6b81bc26ea
leksyib <leksyib14@gmail.com>
2017-11-06 19:50:14 +0100

    fixed controller errors

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ const app = express();
 const bodyParser = require('body-parser');
 const path = require('path')
 const routes = require("./routes/index");
+const flash = require("connect-flash");
 const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("views", path.join(__dirname, "views"));  
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/", routes);
+//app.use(session()); session middleware
+app.use(require("flash")());
 app.listen(port, () => {
     console.log(`App is listening on port: ${port}`);
 });
+
+
+module.exports = app;
---
+++ controllers/routesController.js
@@ -0,0 +1,24 @@
+exports.home = (req, res) => {
+    const title = 'Fun Comments!'
+    res.render('index', title);
+    req.flash("info", `welcome to ${title}`);
+};
+
+exports.signup = (req, res) => {
+    res.render('sign-up', {title: 'Sign up'});
+    req.flash("info", `Fill in the sign-up form to get in!`);
+};
+
+exports.login = (req, res) => {
+    res.render('login', {title: 'login'});
+    req.flash("info", `Enter your username and your password!`);
+};
+
+exports.error = (req, res) => {
+    res.render('error', {title: 'error!'});
+};
+
+exports.profile = (req, res) => {
+    const title = `${user.name}'s profile`;
+    res.render('userprofile', );
+};
